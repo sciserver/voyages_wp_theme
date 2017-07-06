35635c64e6bec805e535640437ab267428df23fe
bonbons0220 <bsouter@jhu.edu>
2017-07-06 11:00:46 -0400

    fixed error from using getimagesize on url instead of path

--- wproto/helper/front.php
+++ wproto/helper/front.php
@@ -534,6 +534,7 @@ class wpl_galaxy_wp_front {
 	 **/
 	public static function logo() {
 		global $wpl_galaxy_wp;
+		$wp_upload_dir = wp_upload_dir();
 
 		$header_logo = $wpl_galaxy_wp->get_option( 'header_logo', 'general' );
 		$logo_type = $wpl_galaxy_wp->get_option( 'logo_type', 'general' );
@@ -549,7 +550,7 @@ class wpl_galaxy_wp_front {
 			$custom_logo_url = $wpl_galaxy_wp->get_option( 'custom_logo_url', 'general' );
 			$custom_logo_url_2x = $wpl_galaxy_wp->get_option( 'custom_logo_url_2x', 'general' );
 			$custom_logo_image = wpl_galaxy_wp_utils::is_retina() ? $custom_logo_url_2x : $custom_logo_url;
-			$size = getimagesize( $custom_logo_url );
+			$size = getimagesize(  ABSPATH . '/' . parse_url($custom_logo_url, PHP_URL_PATH) );
 		?>
 			<a id="logo" <?php if( !is_front_page() ): ?> href="<?php echo site_url(); ?>"<?php endif; ?>><img width="<?php echo isset( $size[0] ) ? $size[0] : ''; ?>" src="<?php echo @$custom_logo_image; ?>" alt="" /></a>
 		<?php
